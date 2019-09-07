31e8f83d163dca1a286b5573bae780361fe1b7f3
nwgat <github@nwgat.net>
2019-09-07 17:04:14 +0200

    Update drop.720p.12mbps.amf.bat

--- drop.720p.12mbps.amf.bat
+++ drop.720p.12mbps.amf.bat
@@ -1,2 +1,2 @@
 @echo off
-ffmpeg -i "%~1" -vf "transpose=1, scale=720:1280" -c:v h264_amf -b:v 6M "%~1".vertical.720p.mp4
+ffmpeg -i "%~1" -vf "transpose=1, scale=720:1280" -c:v h264_amf -b:v 6M -c:a aac -b:a 192K "%~1".vertical.720p.mp4
